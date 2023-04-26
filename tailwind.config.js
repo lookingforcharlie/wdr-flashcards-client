6413154ec634bf93ee11a90709eb2ec712b5d0af
lookingforcharlie <charliefeng2020@gmail.com>
2023-04-25 23:54:27 -0400

    ready for vercel deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
